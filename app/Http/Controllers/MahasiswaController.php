9f91460faa73cbd09537c0962cefe950d52ab077
firyalnajla15 <firyalnajla26@gmail.com>
2026-03-31 15:21:44 +0700

    Create MahasiswaController.php

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        return "Ini adalah method index di MahasiswaController";
+    }
+}
