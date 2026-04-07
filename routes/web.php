2c65c79098b45e1982a807cfeef723d46259c31a
firyalnajla15 <firyalnajla26@gmail.com>
2026-04-07 15:29:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/', function () { //memanggil sebuah kelas, :: = methot static, '/' 
 //Route::get('/mahasiswa',[MahasiswaController::class,'index']);
 Route::get('/dosen',[DosenController::class,'index']);
 
+Route::get('/insert-sql', [MahasiswaController::class, 'insertSql']);
+
 /*Route::get('/home', function(){
     echo "Halaman Home <br>";
     echo "Baris Kedua";
@@ -79,3 +81,4 @@ Route::get('/pnp/{jurusan}/{prodi}', function($jurusan,$prodi){
     $data = [$jurusan,$prodi];
     return view('akademik.prodi')-> with('data',$data);
 })->name('prodi');*/
+
