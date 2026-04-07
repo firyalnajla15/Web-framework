96e0ad213b336ea0f57201222778934f0189811f
firyalnajla15 <firyalnajla26@gmail.com>
2026-04-07 15:28:56 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
-    public function index()
-    {
-        return "Ini adalah method index di MahasiswaController";
+     public function insertSql(){
+        $query=DB::insert("INSERT INTO students (nim, nama_lengkap, tempat_lahir, tgl_lahir, email,
+        prodi, alamat, created_at, updated_at)
+        VALUES ('2401092007', 'Firyal Najla', 'Bukittinggi', '2006-02-15', 'najla@gmail.com', 'Teknologi Informasi',
+         'Jl. jalan jalan jalan', now(), now())");
     }
 }
